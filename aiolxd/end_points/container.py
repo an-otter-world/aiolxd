5fdfccf4e394d9fa2270cf5ba2aa8e2a49c6d21d
AnOtterWorld <59399648+an-otter-world@users.noreply.github.com>
2019-12-21 11:30:38 +0100

    Added containers end points.

---
+++ aiolxd/end_points/container.py
@@ -0,0 +1,5 @@
+"""1.0/container/{name} LXD API endpoint."""
+from aiolxd.core.api_object import ApiObject
+
+class Container(ApiObject):
+    """/1.0/containers/{name} LXD API end point."""
--- aiolxd/end_points/containers.py
+++ aiolxd/end_points/containers.py
@@ -1,6 +1,8 @@
-"""1.0/certificates/* LXD API endpoint & objects."""
+"""1.0/containers LXD API endpoint."""
 from aiolxd.core.iterable_end_point import IterableEndPoint
+from aiolxd.end_points.container import Container
 
 class Containers(IterableEndPoint):
     """/1.0/containers LXD API end point."""
     url = '/1.0/containers'
+    child_class = Container
